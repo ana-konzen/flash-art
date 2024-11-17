a720fd55146839dd72b53497d3e644c9b047c704
ana-konzen <143814540+ana-konzen@users.noreply.github.com>
2024-11-17 16:50:55 -0500

    update

--- public/script.js
+++ public/script.js
@@ -3,7 +3,6 @@ const imageInput = document.getElementById("imageInput");
 const loadingBuffer = document.getElementById("loading");
 const artistFirstName = document.getElementById("artistFirstName");
 const artistLastName = document.getElementById("artistLastName");
-const artistList = document.getElementById("artistList");
 
 const artImg = document.getElementById("artImg");
 const fileLabel = document.getElementById("fileLabel");
@@ -50,15 +49,12 @@ async function sendImage() {
         body: JSON.stringify({ image: reader.result, artist: artistName }),
       });
       const artData = await response.json();
-      // artImg.src = reader.result;
-      // infoCont.innerHTML = artData[0].query;
       console.log(artData);
 
       fileLabel.style.background = "black";
       fileLabel.innerHTML = "choose file";
       loadingBuffer.style.display = "none";
-      // const artCanvas = new p5(s);
-      // artCanvas.allData = artData;
+
       await getArtists();
     } catch (error) {
       console.error(error);
@@ -70,13 +66,14 @@ async function getArtists() {
   console.log("getting artists");
   const result = await fetch("/api/artists");
   const artists = await result.json();
-  console.log(artists);
-  createArtistInfo(artists[artists.length - 1]);
-  // artCanvas.artData = artists[artists.length - 1];
+  indexes = [artists.length - 1];
+  createArtistInfo(artists);
   listArtists(artists);
+  createGenerator(artists);
 }
 
 function listArtists(artists) {
+  const artistList = document.getElementById("artistList");
   artistList.innerHTML = "";
   artists.forEach((artist, index) => {
     const artistBtn = document.createElement("button");
@@ -84,17 +81,64 @@ function listArtists(artists) {
     artistBtn.classList.add("copy");
     artistList.appendChild(artistBtn);
     artistBtn.addEventListener("click", () => {
+      indexes = [index];
+      artCanvas = new p5(logoLayer);
+      artCanvas.allData = artists;
+      artCanvas.seedNumber = Math.floor(Math.random() * 9999999);
+      createArtistInfo(artists);
+    });
+  });
+}
+
+function createGenerator(artists) {
+  const minusCol = document.getElementById("minusCol");
+  const plusCol = document.getElementById("plusCol");
+  const labelCol = document.getElementById("labelCol");
+  labelCol.innerHTML = "";
+  minusCol.innerHTML = "";
+  plusCol.innerHTML = "";
+  artists.forEach((artist, index) => {
+    const artistLabel = document.createElement("div");
+    artistLabel.innerHTML = artist.artist;
+    labelCol.appendChild(artistLabel);
+    const plusButton = document.createElement("button");
+    plusButton.innerHTML = "+";
+    plusCol.appendChild(plusButton);
+    const minusButton = document.createElement("button");
+    minusButton.innerHTML = "-";
+    minusCol.appendChild(minusButton);
+    minusButton.disabled = true;
+    minusButton.classList.add("disabled", "minus");
+
+    plusButton.addEventListener("click", () => {
       indexes.push(index);
       artCanvas = new p5(logoLayer);
       artCanvas.allData = artists;
       artCanvas.seedNumber = 50;
       createArtistInfo(artists);
+      minusButton.disabled = false;
+      minusButton.classList.remove("disabled");
+    });
+
+    minusButton.addEventListener("click", () => {
+      indexes.pop();
+      artCanvas = new p5(logoLayer);
+      artCanvas.allData = artists;
+      artCanvas.seedNumber = 50;
+      if (indexes.length > 0) {
+        createArtistInfo(artists);
+      }
+      if (indexes.length < 1) {
+        minusButton.disabled = true;
+        minusButton.classList.add("disabled");
+      }
     });
   });
 }
 
 function createArtistInfo(artists) {
   const artist = artists[indexes[indexes.length - 1]];
+  console.log(artist);
 
   const artistName = document.getElementById("artistName");
   const palette = document.getElementById("palette");
@@ -117,10 +161,12 @@ function createArtistInfo(artists) {
   }
   mood.innerHTML += `&nbsp &nbsp &nbsp sum: ${artist.afinn.sum}`;
 
+  styles.innerHTML = "";
   for (const style of artist.style) {
     styles.innerHTML += `${style}<br>`;
   }
 
+  shapes.innerHTML = "";
   for (const shape of artist.shape) {
     shapes.innerHTML += `${shape}<br>`;
   }
